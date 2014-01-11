c4e43ad56b9b15fe7b12e76d195c9eec2fa0c536
sanfords <sanford-github@selznick.com>
2014-01-11 13:18:57 -0700

    fileMode 2, text, noise and noiseColor
    Thank you for a perfectly simple and wonderful captcha generator.
    
    1. I create a binary fileMode so source can pull captcha directly with <img src="/someRestInterface" />  I left the option named 'fileMode' to not break backwards compatibility.
    
    2. I created an optional config.text option for times when the captcha value is known ahead of time.
    
    3. Added noise and noiseColor options.

--- captcha.js
+++ captcha.js
@@ -1,7 +1,7 @@
 var Canvas = require('canvas');
 
 module.exports = function(config, callback){
-	config.fileMode   = config.fileMode || false;
+    config.fileMode   = config.fileMode || 0;
     config.size       = config.size || 4;
     config.height     = config.height || 24;
     config.width      = config.width || config.height * config.size;
@@ -9,47 +9,57 @@ module.exports = function(config, callback){
     config.background = config.background || 'rgb(255,255,255)';
     config.lineWidth  = config.lineWidth || 2;
     config.saveDir    = config.saveDir || __dirname;
+    config.text       = config.text || ('' + Math.random()).substr(2, config.size);
+    config.noise      = config.noise || true;
+    config.noiseColor = config.noiseColor || config.color;
     
     var fontSize = Math.round(config.height*0.7);
     var canvas = new Canvas(config.width, config.height);
-	var ctx = canvas.getContext('2d');
-	ctx.fillStyle = config.background;
-	ctx.fillRect(0, 0, config.width, config.height);
-	ctx.fillStyle = config.color;
-	ctx.lineWidth = config.lineWidth;
-	ctx.strokeStyle = config.color;
-	ctx.font = fontSize +'px sans';
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = config.background;
+    ctx.fillRect(0, 0, config.width, config.height);
+    ctx.fillStyle = config.color;
+    ctx.lineWidth = config.lineWidth;
+    ctx.font = fontSize +'px sans';
 
-	for (var i = 0; i < 2; i++) {
-		ctx.moveTo(20, Math.random() * 150);
-		ctx.bezierCurveTo(80, Math.random() * 150, 160, Math.random() * 150, 230, Math.random() * 150);
-		ctx.stroke();
-	}
-
-	var text = ('' + Math.random()).substr(2, config.size);
-
-	for (i = 0; i < text.length; i++) {
-		ctx.setTransform(Math.random() * 0.5 + 1, Math.random() * 0.5, Math.random() * 0.5, Math.random() * 0.5 + 1, config.height * i + (config.height-fontSize)/2, config.height-(config.height-fontSize)/2);
-		ctx.fillText(text.charAt(i), 0, 0);
-	}
+    if (config.noise) {
+        ctx.strokeStyle = config.noiseColor;
+        for (var i = 0; i < 2; i++) {
+            ctx.moveTo(20, Math.random() * 150);
+            ctx.bezierCurveTo(80, Math.random() * 150, 160, Math.random() * 150, 230, Math.random() * 150);
+            ctx.stroke();
+        }
+    }
+    
+    ctx.strokeStyle = config.color;
+    for (i = 0; i < config.text.length; i++) {
+        ctx.setTransform(Math.random() * 0.5 + 1, Math.random() * 0.5, Math.random() * 0.5, Math.random() * 0.5 + 1, config.height * i + (config.height-fontSize)/2, config.height-(config.height-fontSize)/2);
+        ctx.fillText(config.text.charAt(i), 0, 0);
+    }
    
-    if(config.fileMode){
-    	var fs = require('fs');
-    	var filename = new Date().getTime() + Math.floor(Math.random()*1000) +'.png';
-    	var out = fs.createWriteStream(config.saveDir  +"/"+ filename);
-		var stream = canvas.pngStream();
+    if (1 == config.fileMode) {
+        var fs = require('fs');
+        var filename = new Date().getTime() + Math.floor(Math.random()*1000) +'.png';
+        var out = fs.createWriteStream(config.saveDir  +"/"+ filename);
+        var stream = canvas.pngStream();
 
-		stream.on('data', function(chunk){
-		  out.write(chunk);
-		});
+        stream.on('data', function(chunk){
+          out.write(chunk);
+        });
 
-		stream.on('end', function(){
-		    callback(text, filename);
-		});
-    }else{
-	    canvas.toDataURL('image/png', function(err, data){
-            callback(text, data);
-	    });
+        stream.on('end', function(){
+            callback(config.text, filename);
+        });
+    }
+    else if (2 == config.fileMode) {
+        canvas.toBuffer(function(err, buf) {
+            callback(config.text, buf);
+        });
+    }
+    else {
+        canvas.toDataURL('image/png', function(err, data){
+            callback(config.text, data);
+        });
     }
 
 };
